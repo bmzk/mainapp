013d9b47837caed44946d7dda55ca2753b759ee6
techswarn <tech.swarns@gmail.com>
2022-01-30 02:42:56 -0800

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import hero from "./img/hero-img1.jpg"
+import React, { Fragment } from 'react';
+
+import Hotel from './Hotel/Hotels';
+
+
+import hero from "./img/hero-img3.jpg"
 
 export default function Home() {
     return (
-        <div className="section-main">
-            <div className="main">
-                <div className="hero-text-box">
-                    <h1 className="heading-primary">
-                        Personal blog
-                    </h1>
-                    <p className="main-description">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, facilis.
-                    </p>
-                </div>
-                <div className="main-img-box">
-                    <picture>
-                        <img src={hero} alt="my image" className="main-img"/>    
-                    </picture>
+        <Fragment>
+                <div className="section-main">
+                <div className="main">
+                    <div className="hero-text-box">
+                        <h1 className="heading-primary">
+                            Travel | Food && stays
+                        </h1>
+                        <p className="main-description">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, facilis.
+                        </p>
+                    </div>
+                    <div className="main-img-box">
+                        <div className="image-stack">
+                            <div className="image-stack__item image-stack__item-top ">
+                            <picture>
+                                <img src={hero} alt="my image" className="main-img"/>    
+                            </picture>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="head">
+                <h2 className="heading">Our partnerd stays</h2>
+            </div>
+            <Hotel/>
+
+        </Fragment>
+
+
     )
 }
--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import PostCard from './PostCard'
 
 export default function Posts() {
     return (
-        <div>
-            <h1>blog</h1>
+        <div className="blog-container">
+            <div className="blog-posts">
+                <PostCard/>
+                <PostCard/>
+                <PostCard/>
+            </div>
+            <div className="blog-categories">
+                <h3>Blog categories</h3>
+            </div>
         </div>
     )
 }
